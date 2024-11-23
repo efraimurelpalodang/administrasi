de4ae253405f887d22ea366584600c797b3b8af5
efraim urel <efraimurelpalodang@gmail.com>
2024-11-23 08:09:39 +0800

    membuat file login.php didalam file dirpath controllers

--- app/core/App.php
+++ app/core/App.php
@@ -1,6 +1,10 @@
 <?php
 
 class App {
+  protected $controller = 'login';
+  protected $method = 'index';
+  protected $params = [];
+
   public function __construct() {
     $url = $this->pasreURL();
     var_dump($url);
@@ -9,7 +13,9 @@ class App {
   public function pasreURL()
   {
     if( isset($_GET['url']) ) {
-      $url = $_GET['url'];
+      $url = rtrim($_GET['url'], '/');
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
       return $url;
     }
   }
