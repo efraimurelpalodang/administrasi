0c0736d0313e6f651805d1200bda4e6279d875bd
efraim urel <efraimurelpalodang@gmail.com>
2024-11-23 09:17:52 +0800

    membuat halaman login belum setengah

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,11 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Halaman Login</title>
-</head>
-<body>
-  <h1>Selamat datang dihalaman login</h1>
-</body>
-</html>
+<div class="container" id="container">
+  <div class="form-container sign-up">
+    <form action="" method="post">
+      <header>
+        <h1>Buat Akun</h1>
+      </header>
+      <main>
+        <ul>
+          <li><a href="#"><i class="fa-brands fa-google-plus-g"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-github"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
+        </ul>
+        <span>atau gunakan akun email untuk registrasi</span>
+        <input type="text" name="" id="" placeholder="nama">
+        <input type="email" name="" id="" placeholder="email">
+        <input type="password" name="" id="" placeholder="password">
+        <button type="submit">Buat Akun</button>
+      </main>
+    </form>
+  </div>
+</div>
